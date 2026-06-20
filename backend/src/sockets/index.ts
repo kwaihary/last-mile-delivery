d6f14bc2e4e2bd05eef31af7120d8ce4aa73db00
kwaihary <nloc5454@gmail.com>
2026-06-20 22:58:13 +0700

    feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,2 +1,3 @@
 // Rong
-//feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals 
+//feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals
+//feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals - TẠO PULL REQUEST
