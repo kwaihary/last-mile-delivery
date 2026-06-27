be598847662f717c133a2879c55c2928f4177b57
kwaihary <nloc5454@gmail.com>
2026-06-27 09:03:01 +0700

    mock test redis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,43 @@
-// feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals 
+// feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals  
+import express from 'express';
+import 'reflect-metadata';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import redisClient from './config/redis';
+import cors from 'cors';
+import { initSockets } from './sockets';
+// Load biến môi trường
+dotenv.config();
+
+// Khởi tạo ứng dụng express
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Tạo HTTP dùng cho SocketIO
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
+
+initSockets(io);
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'OK',
+        message: 'Server is working nicely!'
+    });
+});
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+});
--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,3 +1,40 @@
-// Rong
-//feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals
-//feat(socket): initiate socket.io gateway handler for high-frequency geolocation signals - TẠO PULL REQUEST
+import { Server } from 'socket.io'
+import redisClient from '../config/redis';
+export const initSockets = (io: Server) => {
+    
+    io.on('connection', (socket) => {
+        console.log(`📡 Thiết bị kết nối Gateway thành công: ${socket.id}`);
+
+        socket.on('driver:share-location', async (data: { orderId: number, driverId: number, lat: number, lng: number }) => {
+            const { orderId, driverId, lat, lng } = data;
+            if (!lat || !lng || !driverId) return;
+
+            try {
+                const currentTimestamp = String(Math.floor(Date.now() / 1000));
+
+                // Lưu vị trí của tài xế
+                await redisClient.geoadd('drivers:locations', lng, lat, String(driverId));
+
+                // Lưu trạng thái của tài xế
+                await redisClient.hset(
+                    `driver:status:${driverId}`,
+                    'current_status', 'delivering',
+                    'active_order_id', String(orderId || ''),
+                    'last_ping', currentTimestamp
+                );
+
+                // Lưu lịch sử chuyến đi
+                if (orderId) {
+                    const locationPayload = JSON.stringify({ lat, lng, t: Number(currentTimestamp) });
+                    await redisClient.rpush(`order:route:${orderId}`, locationPayload);
+                }
+
+                console.log(`Tài xế [${driverId}] -> Đơn [${orderId}]: Đã đồng bộ dữ liệu.`);
+            } catch (error) {
+                console.error('Lỗi khi ghi dữ liệu vào Redis:', error);
+            }
+        });
+
+        socket.on('disconnect', () => console.log(`Thiết bị ngắt kết nối: ${socket.id}`));
+    });
+}
